f657cfdfaf316e63e334bf90f98842d62ee7801a
ignatIgnatov <ignatov_rs@abv.bg>
2024-06-22 15:58:23 +0300

    add .env.local file

--- src/database/index.js
+++ src/database/index.js
@@ -3,7 +3,7 @@ const { default: mongoose } = require("mongoose");
 
 
 const connectToDB = async () => {
-    const url = 'mongodb+srv://promptopia:twWhwov7pt4mRZEw@cluster0.hiuu2xs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+    const url = process.env.DATABASE_URL;
 
     mongoose.connect(url)
         .then(() => console.log('Database connected successfully!'))
